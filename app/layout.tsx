2bc59d4f2bf2f9aca8a307f4a44ff7917c7280b3
angelahack1 <angelakimichellle@hotmail.com>
2025-07-16 18:45:11 -0600

    Trying to fix on mobiles, by angelahack1

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </head>
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
       >
